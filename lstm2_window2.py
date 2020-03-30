31c232bf147acdf464ffa6c7c84bb4760e55eb0f
sayantanibasu <58863505+sayantanibasu@users.noreply.github.com>
2020-03-30 08:36:10 -0500

    Add files via upload

--- lstm2_window2.py
+++ lstm2_window2.py
@@ -16,13 +16,13 @@ data = pandas.read_excel('Country_Cases.xlsx')
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 country='US'
-lookback=3
-lookahead=2
+lookback=1
+lookahead=1
 
-train_dates=['J20','J21','J23','J24','J25','J26','J27','J28','J29','J30','J31','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','F13','F14','F15','F16','F17','F18','F19','F20','F21','F22','F23','F24','F25','F26','F27','F28','F29','M1','M2','M3','M4','M5','M6','M7','M8','M9','M10','M11']
-test_dates=['M11','M12','M13','M14','M15','M16','M17','M18','M19','M20','M21','M22']
+train_dates=['J22','J23','J24','J25','J26','J27','J28','J29','J30','J31','F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','F13','F14','F15','F16','F17','F18','F19','F20','F21','F22','F23','F24','F25','F26','F27','F28','F29','M1','M2','M3','M4','M5','M6','M7','M8','M9','M10','M11','M12','M13','M14','M15','M16']
+test_dates=['M16','M17','M18','M19','M20','M21','M22','M23','M24','M25','M26','M27','M28','M29']
 
-testX_total=data[data['Country']==country][test_dates[:-(lookback-1)]].values[0]
+testX_total=data[data['Country']==country][test_dates[:-(lookahead)]].values[0]
 
 testX=[]
 for i in range(len(testX_total)-(lookback-1)):
@@ -32,9 +32,10 @@ for i in range(len(testX_total)-(lookback-1)):
     testX.append(temp)
 
 
-testY=data[data['Country']==country][test_dates[(lookback-1+lookahead):]].values[0]
+testY=data[data['Country']==country][test_dates[(lookback):]].values[0]
 
-trainX_total=data[data['Country']==country][train_dates[:-(lookback-1)]].values[0]
+trainX_total=data[data['Country']==country][train_dates[:-(lookahead)]].values[0]
+
 trainX=[]
 for i in range(len(trainX_total)-(lookback-1)):
     temp=[]
@@ -42,7 +43,7 @@ for i in range(len(trainX_total)-(lookback-1)):
         temp.append(trainX_total[i+j])
     trainX.append(temp)
 
-trainY=data[data['Country']==country][train_dates[(lookback-1+lookahead):]].values[0]
+trainY=data[data['Country']==country][train_dates[(lookback):]].values[0]
 
 p=np.concatenate((testX_total,testY))
 p=np.concatenate((p,trainX_total))
@@ -51,15 +52,20 @@ p=np.concatenate((p,trainY))
 p=p.reshape(-1,1)
 scaler.fit(p)
 
-testX=np.array(testX) 
-#testX=testX.reshape(-1,1)
+testX=np.array(testX)
+if lookback==1:
+    testX=testX.reshape(-1,1)
+
+print(trainX)
+
 testX = scaler.transform(testX)
 
 testY=testY.reshape(-1,1)
 testY = scaler.transform(testY)
 
 trainX=np.array(trainX)
-#trainX=trainX.reshape(-1,1)
+if lookback==1:
+    trainX=trainX.reshape(-1,1)
 trainX = scaler.transform(trainX)
 
 trainY = trainY.reshape(-1,1)
@@ -88,16 +94,16 @@ testY=testY.reshape(-1,1)
 plt.figure(0)
 
 plt.title(country+" - Learning Curve for 80% Training Data") # 80% training data write # how much data
-plt.scatter(train_dates[:-(lookback-1+lookahead)],trainY)
-plt.scatter(train_dates[:-(lookback-1+lookahead)],trainPredict)
+plt.scatter(train_dates[:-(lookahead)],trainY)
+plt.scatter(train_dates[:-(lookahead)],trainPredict)
 
 
 plt.xlabel("Date")
 plt.ylabel("Number of COVID-19 Infections")
-plt.ylim(0,max(trainY)+1000) 
+plt.ylim(0,1.2*max(trainY)) 
 
 
-plt.xticks(np.arange(49), ['','','J25','','','J28','','','J31','','','F3','','','F6','','','F9','','','F12','','','F15','','','F18','','','F21','','','F24','','','F27','','','M1','','','M4','','','M7','','','M10',''])
+plt.xticks(np.arange(len(train_dates)), ['J22','','','J25','','','J28','','','J31','','','F3','','','F6','','','F9','','','F12','','','F15','','','F18','','','F21','','','F24','','','F27','','','M1','','','M4','','','M7','','','M10','','','M13','','','M16'])
 plt.gca().legend(('actual','learnt'))
 plt.savefig(country+"_train_"+"b_"+str(lookback)+"_a_"+str(lookahead)+".png")
 
@@ -105,15 +111,15 @@ plt.savefig(country+"_train_"+"b_"+str(lookback)+"_a_"+str(lookahead)+".png")
 plt.figure(1)
 
 plt.title(country+" - Predictions for Test Data")
-plt.scatter(test_dates[(lookback-1+lookahead):],testY)
-plt.scatter(test_dates[(lookback-1+lookahead):],testPredict)
+plt.scatter(test_dates[(lookback):],testY)
+plt.scatter(test_dates[(lookback):],testPredict)
 
 
 plt.xlabel("Date")
 plt.ylabel("Number of COVID-19 Infections") # discrete rep for training and test
-plt.ylim(0,max(testY)+7000) 
+plt.ylim(0,1.2*max(testY)) 
 
 
-plt.xticks(np.arange(10), ['M13','M14','M15','M16','M17','M18','M19','M20','M21','M22'])
+plt.xticks(np.arange(len(test_dates)+1), ['M16','M17','M18','M19','M20','M21','M22','M23','M24','M25','M26','M27','M28','M29','M30'])
 plt.gca().legend(('actual','predicted'))
 plt.savefig(country+"_test_"+"b_"+str(lookback)+"_a_"+str(lookahead)+".png")
